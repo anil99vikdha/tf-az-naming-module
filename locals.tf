b6f71d46bd6a61fe50271f08b23577f5e6d16158
Anil Kumar Maheswarla <anilkumar.mah999@gmail.com>
2025-10-11 11:10:29 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -5,5 +5,5 @@ resource "random_id" "rand_id" {
 locals {
   res_base_name     = "tf-az"
   resource_name     = "${local.res_base_name}-${var.project_name}-${var.environment}"
-  storage_name_conv = replace("${local.resource_name}${random.rand_id.hex}", "-", "")
+  storage_name_conv = replace("${local.resource_name}${random_id.rand_id.hex}", "-", "")
 }
