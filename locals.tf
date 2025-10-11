233b3d1382ff715ae34611953d4f1456517b007c
Anil Kumar Maheswarla <anilkumar.mah999@gmail.com>
2025-10-11 05:32:45 +0000

    fix: added provider

--- locals.tf
+++ locals.tf
@@ -1,9 +1,9 @@
-resource "random" "rand_id" {
+resource "random_id" "rand_id" {
   byte_length = 2
 }
 
 locals {
   res_base_name     = "tf-az"
   resource_name     = "${local.res_base_name}-${var.project_name}-${var.environment}"
-  storage_name_conv = replace("${local.resource_name}${random.rand_id.hex}", "-", "")
+  storage_name_conv = replace("${local.resource_name}${random_id.rand_id.hex}", "-", "")
 }
